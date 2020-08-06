8fe290693bccbab25cfbabcc9e1780a052d0efff
Jarred Trost <jtrost@gmail.com>
2020-08-06 12:57:56 -0500

    Use pagerating package

--- src/js/trilogy-components/user-feedback/webpack.config.js
+++ src/js/trilogy-components/user-feedback/webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
 
 module.exports = {
-    entry: __dirname + '/styles/index.scss',
+    entry: [
+      __dirname + '/styles/index.scss',
+      __dirname + '/js/index.js'
+    ],
+    output: {
+      path: path.resolve(__dirname, 'dist'), 
+      filename: 'index.min.js'
+    },
     module: {
       rules: [
         {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: []
+        },{
           test: /\.scss$/,
           exclude: /node_modules/,
           use: [
